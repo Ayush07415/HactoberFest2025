cbfcd79ee183f3d151a16ffa4fabd2f1dc7a7adb
Ayush Sarkar <sarkarayush2004@gmail.com>
2025-10-04 00:21:49 +0530

    Create PrintNoDigits.java
    # Print 1 to 100 Without Using Numbers
    
    This Java program prints numbers from 1 to 100  "without using any numeric digits" in the code.  
    It uses string lengths and simple arithmetic to generate the numbers dynamically.

---
+++ java/PrintNoDigits.java
@@ -0,0 +1,15 @@
+//Username= Ayush07415
+//Aim= Binary serach tree
+//Date=4/10/2025
+
+public class PrintNoDigits {
+    public static void main(String[] args) {
+        int one = " ".length();                  
+        int ten = "helloworld".length();         
+        int hundred = ten * ten;                 
+
+        for (int i = one; i <= hundred; i++) {
+            System.out.println(i);
+        }
+    }
+}
