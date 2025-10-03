722e56a384b62abe10996b5e251c15fb3ace5df3
Ayush Sarkar <sarkarayush2004@gmail.com>
2025-10-03 22:26:03 +0530

    Create Fibonacci.java

---
+++ java/Fibonacci.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    private int n; 
+
+ 
+    public Fibonacci(int n) {
+        this.n = n;
+    }
+
+
+    public void generateSeries() {
+        int a = 0, b = 1, c;
+        System.out.println("Fibonacci Series up to " + n + " terms:");
+        for (int i = 1; i <= n; i++) {
+            System.out.print(a + " ");
+            c = a + b;
+            a = b;
+            b = c;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of terms: ");
+        int terms = sc.nextInt();
+
+        Fibonacci fib = new Fibonacci(terms);
+        fib.generateSeries();
+
+        sc.close();
+    }
+}
